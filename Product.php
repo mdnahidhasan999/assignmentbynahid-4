e57ddbc43c40ca1ccd89a773042337966e318707
Md Nahid Hasan <mdnahidhasanmunna999@gmail.com>
2023-10-02 21:29:16 +0600

    commit

---
+++ Product.php
@@ -0,0 +1,32 @@
+<?php
+
+class Product {
+    // Properties
+    private $id;
+    private $name;
+    private $price;
+
+    // Constructor
+    public function __construct($id, $name, $price) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function getPrice() {
+        return '$' . number_format($this->price, 2);
+    }
+    public function showDetails() {
+        echo "Product Details:\n";
+        echo "- ID: {$this->id}\n";
+        echo "- Name: {$this->name}\n";
+        echo "- Price: {$this->getPrice()}\n";
+    }
+}
+
+$product = new Product(1, 'T-shirt', 19.99);
+$product->showDetails();
+
+
+
+
