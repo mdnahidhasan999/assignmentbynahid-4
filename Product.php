78d08bcd97eb537278f99e6ca0270b0a1d62e4dd
Md Nahid Hasan <mdnahidhasanmunna999@gmail.com>
2023-10-02 21:31:21 +0600

    modified

--- Product.php
+++ Product.php
@@ -1,12 +1,10 @@
 <?php
 
 class Product {
-    // Properties
     private $id;
     private $name;
     private $price;
 
-    // Constructor
     public function __construct($id, $name, $price) {
         $this->id = $id;
         $this->name = $name;
